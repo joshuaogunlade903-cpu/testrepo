1865902a2b048683c06c04e3c5c00305a685beb6
Joshua Ogunlade <joshuaogunlade903@gmail.com>
2026-05-01 18:09:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {DOM,APP,ZNativeImport,$bind} = Zenith
 
 Zenith.setGlobal({$bind})
 
-//const temp=ZNativeImport('main')
-//alert(temp)
-alert("hello")
+const temp=ZNativeImport('main')
+alert(temp)
+//alert("hello")
 //temp.runIn(DOM.body)
